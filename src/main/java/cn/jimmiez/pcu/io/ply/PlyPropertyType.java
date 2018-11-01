c2a271037f2da000a0c9f2730c7d5ffec6f9863c
zjl <cquzjl@gmail.com>
2018-11-01 10:06:55 +0800

    add enum type

---
+++ src/main/java/cn/jimmiez/pcu/io/ply/PlyPropertyType.java
@@ -0,0 +1,22 @@
+package cn.jimmiez.pcu.io.ply;
+
+public enum PlyPropertyType {
+
+    NON_TYPE(0),
+    CHAR(1),
+    UCHAR(2),
+    SHORT(3),
+    USHORT(4),
+    INT(5),
+    UINT(6),
+    FLOAT(7),
+    DOUBLE(8),
+    LIST(10);
+
+    private int val = 0;
+
+    PlyPropertyType(int val) {
+        this.val = val;
+    }
+
+}
